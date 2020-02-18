8262024e8b78cd0cceb61578d303ddb5bd9914c9
Thomas Anderson <thomas.anderson@ucdconnect.ie>
2020-02-17 20:03:47 -0700

    Added head meta data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import Counter from "./containers/Counter/Counter";
 
 function App() {
-  return (
+    document.title ="ReactReduxCounter | A small app to demo Redux state changes";
+    document.getElementsByTagName("META")[2].content="A small app to demo React-Redux state changes via props";
+
+    return (
     <div className="App">
       <header className="App-header">
         React-Redux Counter
