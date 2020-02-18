5e3955a4dfbd4d738409f262c60a889d798c2a76
Thomas Anderson <thomas.anderson@ucdconnect.ie>
2020-02-17 19:20:29 -0700

    Updates

---
+++ precache-manifest.70c1d14639eb4a8dbc43126aa5bf9432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aac186e24617f4dab57a6dea811ee27",
+    "url": "/ReactReduxCounter/index.html"
+  },
+  {
+    "revision": "26439c439fb58cef1511",
+    "url": "/ReactReduxCounter/static/css/main.cc4072ae.chunk.css"
+  },
+  {
+    "revision": "0e74cf71199bcad3e14e",
+    "url": "/ReactReduxCounter/static/js/2.d361b0f6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ReactReduxCounter/static/js/2.d361b0f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26439c439fb58cef1511",
+    "url": "/ReactReduxCounter/static/js/main.924a22c1.chunk.js"
+  },
+  {
+    "revision": "fce16eaa2cbd8a83322e",
+    "url": "/ReactReduxCounter/static/js/runtime-main.3759115a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.081764507db75dc1c289c40e03adec00.js"
+  "/ReactReduxCounter/precache-manifest.70c1d14639eb4a8dbc43126aa5bf9432.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactReduxCounter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
